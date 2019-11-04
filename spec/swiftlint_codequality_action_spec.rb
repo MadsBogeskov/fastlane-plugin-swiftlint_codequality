25e26cd93284da0371f890bfbcad2ed06c03a441
Morten Bøgh <morten@justabeech.com>
2019-11-04 10:01:22 +0100

    Fixed intial test

--- spec/swiftlint_codequality_action_spec.rb
+++ spec/swiftlint_codequality_action_spec.rb
@@ -1,9 +1,18 @@
 describe Fastlane::Actions::SwiftlintCodequalityAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The swiftlint_codequality plugin is working!")
+  let(:fixtures_path) { File.expand_path("./spec/fixtures") }
 
-      Fastlane::Actions::SwiftlintCodequalityAction.run(nil)
+  describe '#run' do
+    it 'generates empty output if input is also empty' do
+      path = fixtures_path + "/empty.json"
+      output = fixtures_path + "/empty.result.json"
+
+      expect(Fastlane::UI).to receive(:message).with("Parsing SwiftLint report at #{path}")
+      expect(Fastlane::UI).to receive(:success).with("🚀 Generated Code Quality report at #{output} 🚀")
+
+      Fastlane::Actions::SwiftlintCodequalityAction.run(path: path, output: output)
+
+      result = File.read(output)
+      expect(result).to eq("[]")
     end
   end
 end

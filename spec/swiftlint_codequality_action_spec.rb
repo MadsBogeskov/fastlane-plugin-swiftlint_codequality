672733914dea24b8038d362ed50b8bef4b661b98
Morten Bøgh <morten@justabeech.com>
2019-11-05 08:24:38 +0100

    Added a test for multiple issues

--- spec/swiftlint_codequality_action_spec.rb
+++ spec/swiftlint_codequality_action_spec.rb
@@ -31,5 +31,23 @@ describe Fastlane::Actions::SwiftlintCodequalityAction do
       result = File.read(output)
       expect(result).to eq(%q{[{"type":"issue","check_name":"Identifier Name Violation","description":"Variable name should be between 3 and 40 characters long: 'p'","fingerprint":"684722650e18611b41939c985095d204","severity":"critical","location":{"path":"/Project/File.swift","lines":{"begin":20,"end":20}}}]})
     end
+
+    it 'outputs as many code climate objects as there are swiftlint issues' do
+      path = fixtures_path + "/multiple.txt"
+      output = fixtures_path + "/multiple.result.json"
+
+      expect(Fastlane::UI).to receive(:message).with("Parsing SwiftLint report at #{path}")
+      expect(Fastlane::UI).to receive(:success).with("🚀 Generated Code Quality report at #{output} 🚀")
+
+      command = "pwd"
+      command_result = "/Users/apple/projects"
+      allow(Fastlane::Actions).to receive(:sh).with(command, log: false).and_return(command_result)
+
+      Fastlane::Actions::SwiftlintCodequalityAction.run(path: path, output: output, prefix: '')
+
+      input = File.foreach(path).count
+      result = JSON.parse(File.read(output)).length
+      expect(result).to eq(input)
+    end
   end
 end
